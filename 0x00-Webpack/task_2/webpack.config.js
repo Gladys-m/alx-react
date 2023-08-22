99a4675fe5eb97f9b0fae50fcf6ab95a95dafc17
Gladys-m <gladysmitambo@gmail.com>
2023-08-22 22:25:00 +0400

    Adding CSS & Images

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader', 'image-webpack-loader']
       },
       {
         test: /\.(jpg|jpeg|png|gif)$/,
