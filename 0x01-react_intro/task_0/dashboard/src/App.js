d8d6c85c4ebbd5e87d6c37d336310421a071e2fc
Gladys-m <gladysmitambo@gmail.com>
2023-08-24 08:51:39 +0400

    Basic react application

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,21 @@
+import holbertonLogo from "./holbertonLogo.jpg";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} className="App-logo"/>
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
